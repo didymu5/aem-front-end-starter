2743a5ffac99434e68dd38191737383ed0476b9f
Thomas X. Wu <thomas.wu@kp.org>
2017-09-10 21:50:29 -0700

    removed ts stuff

--- clientlibs/src/ts/polyfill.js
+++
@@ -1,8 +0,0 @@
-
-//-----------------------------------------------------------------------------
-/**
- * Getting ONLY neccessary polyfills from core-js.
- * for list of polyfills see this: https://github.com/zloirock/core-js/tree/62c058c138eb3eedee36bf59534f07cd1594ae6a/fn
- */
-//-----------------------------------------------------------------------------
-require('core-js/fn/string/starts-with');
